e26f5a54388e150d5dafcf61c8299d287bc1a767
Sanjai-Shaarugesh <shaarugesh5@outlook.com>
2024-12-07 23:53:05 +0530

    few customization and bug fixed

--- prefs.js
+++ prefs.js
@@ -1,53 +1,53 @@
 import Adw from 'gi://Adw';
 import Gtk from 'gi://Gtk';
-import { ExtensionPreferences } from 'resource:///org/gnome/shell/extensions/extension.js';
+import { ExtensionPreferences, gettext as _ } from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 export default class WeatherPreferences extends ExtensionPreferences {
     fillPreferencesWindow(window) {
-        const settings = this.getSettings('org.gnome.shell.extensions.advaced-weather');
+        const settings = this.getSettings('org.gnome.shell.extensions.advanced-weather');
 
         const page = new Adw.PreferencesPage();
-        const group = new Adw.PreferencesGroup({ title: 'Weather Settings' });
+        const group = new Adw.PreferencesGroup({ title: _('Weather Settings') });
 
-       
+        
         const locationModeRow = new Adw.ActionRow({ 
-            title: 'Location Mode', 
-            subtitle: 'Choose how location is determined' 
+            title: _('Location Mode'), 
+            subtitle: _('Choose how location is determined') 
         });
         const locationModeCombo = new Gtk.ComboBoxText();
-        locationModeCombo.append('auto', 'Auto Detect');
-        locationModeCombo.append('manual', 'Manual Setup');
-        
+        locationModeCombo.append('auto', _('Auto Detect'));
+        locationModeCombo.append('manual', _('Manual Setup'));
+
         const currentMode = settings.get_string('location-mode') || 'auto';
-        locationModeCombo.active_id = currentMode;
-        
+        locationModeCombo.set_active_id(currentMode);
+
         locationModeRow.add_suffix(locationModeCombo);
         group.add(locationModeRow);
 
         locationModeCombo.connect('changed', (widget) => {
-            settings.set_string('location-mode', widget.active_id);
+            settings.set_string('location-mode', widget.get_active_id());
         });
 
         
         const locationRow = new Adw.ActionRow({ 
-            title: 'Manual Location', 
-            subtitle: 'City name or coordinates' 
+            title: _('Manual Location'), 
+            subtitle: _('City name or coordinates') 
         });
         const locationEntry = new Gtk.Entry({
             text: settings.get_string('location') || '',
-            placeholder_text: 'New York or 40.7128,-74.0060'
+            placeholder_text: _('New York or 40.7128,-74.0060')
         });
         locationRow.add_suffix(locationEntry);
         group.add(locationRow);
 
         locationEntry.connect('changed', () => {
-            settings.set_string('location', locationEntry.text);
+            settings.set_string('location', locationEntry.get_text());
         });
 
         
         const unitRow = new Adw.ActionRow({ 
-            title: 'Temperature Unit', 
-            subtitle: 'Choose between Celsius and Fahrenheit' 
+            title: _('Temperature Unit'), 
+            subtitle: _('Choose between Celsius and Fahrenheit') 
         });
         const unitSwitch = new Gtk.Switch({
             active: settings.get_boolean('use-fahrenheit') || false
